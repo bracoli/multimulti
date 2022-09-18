a9750f99c320af28f24c36ce517c081de49d60b0
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-19 04:18:48 +0700

    Update install-sldns.sh

--- slowdnss/install-sldns.sh
+++ slowdnss/install-sldns.sh
@@ -33,80 +33,102 @@ service cron reload
 service cron restart
 service iptables reload
 
-rm -rf /etc/slowdns
-mkdir -m 777 /etc/slowdns
-wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/server.key"
-wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/server.pub"
-wget -q -O /etc/slowdns/sldns-server "https://github.com/khairunisya/slowdns-client-server/raw/main/sldns-server"
-wget -q -O /etc/slowdns/sldns-client "https://github.com/khairunisya/slowdns-client-server/raw/main/sldns-client"
-cd
-chmod +x /etc/slowdns/server.key
-chmod +x /etc/slowdns/server.pub
-chmod +x /etc/slowdns/sldns-server
-chmod +x /etc/slowdns/sldns-client
-cd
-wget -q -O /etc/systemd/system/client-sldns.service "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/client-sldns.service"
-wget -q -O /etc/systemd/system/server-sldns.service "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/server-sldns.service"
-cd
+cd /usr/local
+wget https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
+tar xvf go1.16.2.linux-amd64.tar.gz
+export GOROOT=/usr/local/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+cd /root
+apt install git -y
+git clone https://www.bamsoftware.com/git/dnstt.git temp
+mv /root/temp /root/dnstt
+rm -rf temp
+cd /root/dnstt/dnstt-server
+go build
+./dnstt-server -gen-key -privkey-file /root/dnstt/dnstt-server/server.key -pubkey-file /root/dnstt/dnstt-server/server.pub
+sudo sed -i 's/AllowTcpForwarding no/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+echo "AllowTcpForwarding yes" >> /etc/ssh/sshd_config
+service ssh restart
+sudo echo "screen -dmS slowdns iptables -I INPUT -p udp --dport 5300 -j ACCEPT && iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300 && ulimit -u unlimited && ulimit -n 65536 && /root/dnstt/dnstt-server/dnstt-server -udp :5300 -privkey-file /root/dnstt/dnstt-server/server.key $nameserver 127.0.0.1:443" > /bin/slow
+sudo chmod +x /bin/slow
+clear
+cat /root/dnstt/dnstt-server/server.pub
+
+
+#rm -rf /etc/slowdns
+#mkdir -m 777 /etc/slowdns
+#wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/server.key"
+#wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/server.pub"
+#wget -q -O /etc/slowdns/sldns-server "https://github.com/khairunisya/slowdns-client-server/raw/main/sldns-server"
+#wget -q -O /etc/slowdns/sldns-client "https://github.com/khairunisya/slowdns-client-server/raw/main/sldns-client"
+#cd
+#chmod +x /etc/slowdns/server.key
+#chmod +x /etc/slowdns/server.pub
+#chmod +x /etc/slowdns/sldns-server
+#chmod +x /etc/slowdns/sldns-client
+#cd
+#wget -q -O /etc/systemd/system/client-sldns.service "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/client-sldns.service"
+#wget -q -O /etc/systemd/system/server-sldns.service "https://raw.githubusercontent.com/khairunisya/multiws/main/slowdnss/server-sldns.service"
+#cd
 #install client-sldns.service
-cat > /etc/systemd/system/client-sldns.service << END
-[Unit]
-Description=Client SlowDNS By Jrtunnel
-Documentation=https://www.jrtunnel.com
-After=network.target nss-lookup.target
+#cat > /etc/systemd/system/client-sldns.service << END
+#[Unit]
+##Description=Client SlowDNS By Jrtunnel
+#Documentation=https://www.jrtunnel.com
+#After=network.target nss-lookup.target
 
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/etc/slowdns/sldns-client -udp 8.8.8.8:53 --pubkey-file /etc/slowdns/server.pub $nameserver 127.0.0.1:3369
-Restart=on-failure
+#[Service]
+#Type=simple
+#User=root
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#NoNewPrivileges=true
+#ExecStart=/etc/slowdns/sldns-client -udp 8.8.8.8:53 --pubkey-file /etc/slowdns/server.pub $nameserver 127.0.0.1:3369
+#Restart=on-failure
 
-[Install]
-WantedBy=multi-user.target
-END
-cd
+#[Install]
+#WantedBy=multi-user.target
+#END
+#cd
 #install server-sldns.service
-cat > /etc/systemd/system/server-sldns.service << END
-[Unit]
-Description=Server SlowDNS By Jrtunnel
-Documentation=https://www.jrtunnel.com
-After=network.target nss-lookup.target
+#cat > /etc/systemd/system/server-sldns.service << END
+#[Unit]
+#Description=Server SlowDNS By Jrtunnel
+#Documentation=https://www.jrtunnel.com
+#After=network.target nss-lookup.target
 
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:2269
-Restart=on-failure
+#[Service]
+#Type=simple
+#User=root
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#NoNewPrivileges=true
+#ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:2269
+#Restart=on-failure
 
-[Install]
-WantedBy=multi-user.target
-END
-cd
-chmod +x /etc/systemd/system/client-sldns.service
-chmod +x /etc/systemd/system/server-sldns.service
-pkill sldns-server
-pkill sldns-client
+#[Install]
+#WantedBy=multi-user.target
+#END
+#cd
+#chmod +x /etc/systemd/system/client-sldns.service
+##chmod +x /etc/systemd/system/server-sldns.service
+#pkill sldns-server
+#pkill sldns-client
 
-iptables -I INPUT -p udp --dport 5300 -j ACCEPT
-iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
+#iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+#iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+#iptables-save > /etc/iptables.up.rules
+#iptables-restore -t < /etc/iptables.up.rules
+#netfilter-persistent save
+#netfilter-persistent reload
 
-systemctl daemon-reload
-systemctl stop client-sldns
-systemctl stop server-sldns
-systemctl enable client-sldns
-systemctl enable server-sldns
-systemctl start client-sldns
-systemctl start server-sldns
-systemctl restart client-sldns
-systemctl restart server-sldns
+#systemctl daemon-reload
+#systemctl stop client-sldns
+#systemctl stop server-sldns
+#systemctl enable client-sldns
+#vsystemctl enable server-sldns
+#systemctl start client-sldns
+#vsystemctl start server-sldns
+#systemctl restart client-sldns
+#systemctl restart server-sldns
 cd
