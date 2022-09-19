f37fb4bb2b950889033eb75eda336987f92e4c71
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-20 03:06:28 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -203,13 +203,13 @@ sleep 2
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -qc -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/khairunisya/multiws/main/ssh/nginx.conf"
-#rm /etc/nginx/conf.d/vps.conf
+rm /etc/nginx/conf.d/vps.conf
 wget -qc -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/khairunisya/multiws/main/ssh/vps.conf"
 /etc/init.d/nginx restart
 echo -e "[ ${green}INFO$NC ] Setting"
 mkdir /etc/systemd/system/nginx.service.d
 printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
-rm /etc/nginx/conf.d/default.conf
+#rm /etc/nginx/conf.d/default.conf
 systemctl daemon-reload
 service nginx restart
 echo -e "[ ${green}INFO${NC} ] Success... "
