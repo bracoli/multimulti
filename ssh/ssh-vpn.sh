a002a60ed4a320e6010a49263ddd0ea401a1e515
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-20 01:38:05 +0700

    up

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -10,6 +10,67 @@ NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 source /etc/os-release
 ver=$VERSION_ID
 
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/khairunisya/permission/main/ipmini > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/khairunisya/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/khairunisya/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+clear
+
 #detail nama perusahaan
 country=ID
 state=Indoneisa
@@ -19,25 +80,7 @@ organizationalunit=Jrtunnel
 commonname=none
 email=admin@jrtunnel.com
 
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-NC='\e[0m'
-echo "Progress..."
-sleep 3
-#green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-#red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-#PERMISSION
-#if [ "$res" = "Permission Accepted..." ]; then
-#green "Permission Accepted.."
-#else
-#red "Permission Denied!"
-#exit 0
-#fi
-#echo -e "
-#"
-date
-echo ""
+
 echo -e "[ ${green}INFO${NC} ] Installer SSH... "
 # simple password minimal
 curl -sS https://raw.githubusercontent.com/khairunisya/multiws/main/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
@@ -220,6 +263,7 @@ echo -e " [INFO] Successfully"
 sleep 1
 cd
 echo -e "[ ${green}INFO$NC ] Setting"
+sleep 2
 # install stunnel
 #apt install stunnel4 -y
 cat > /etc/stunnel/stunnel.conf <<-END
@@ -258,11 +302,10 @@ echo -e "[ ${green}INFO$NC ] Setting"
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 /etc/init.d/stunnel4 restart
 
-
+echo -e "[ ${green}INFO${NC} ] Installer DDOs Protect... "
+sleep 2
 # install fail2ban
 apt -y install fail2ban
-sleep 1
-echo -e "[ ${green}INFO${NC} ] Installer DDOs Protect... "
 # Instal DDOS Flate
 if [ -d '/usr/local/ddos' ]; then
 	echo; echo; echo "Please un-install the previous version first"
@@ -294,14 +337,15 @@ sleep 1
 # banner /etc/issue.net
 sleep 1
 echo -e "[ ${green}INFO$NC ] Settings banner"
+sleep 1
 wget -qc -O /etc/issue.net "https://raw.githubusercontent.com/khairunisya/multiws/main/issue.net"
 chmod +x /etc/issue.net
 echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 echo -e "[ ${green}INFO$NC ] Setting"
+sleep 1
 # download script
 cd /usr/bin
-sleep 1
 echo -e "[ ${green}INFO${NC} ] Downloading files... "
 wget -qc -O speedtest "https://github.com/SSHSEDANG4/speedtest-cli/raw/main/speedtest_cli.py"
 wget -qc -O xp "https://raw.githubusercontent.com/khairunisya/multiws/main/ssh/xp.sh"
